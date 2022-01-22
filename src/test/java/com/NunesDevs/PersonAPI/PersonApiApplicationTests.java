61de691eb5c827f2fa57aab962ff177d2900f751
NunesDevs <matheusnunes370@gmail.com>
2022-01-22 12:05:28 -0300

    created repository

---
+++ src/test/java/com/NunesDevs/PersonAPI/PersonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NunesDevs.PersonAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
